e2478b636862b8f12487c25f63bca748ae2d9087
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-04-10 13:11:39 -0300

    feat: req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    news.sort(
+        key=lambda new: new['shares_count'] + new['comments_count']
+        )
+
+    news.reverse()
+
+    top_5_news = []
+
+    for new in news[:5]:
+        top_5_news.append((new['title'], new['url']))
+
+    return top_5_news
 
 
 # Requisito 11
